63a5f5c95f0bf8de426b95197f273f05150fc13a
Prabhatroy03 <101659036+Prabhatroy03@users.noreply.github.com>
2022-06-12 10:28:00 +0530

    Add files via upload

---
+++ Circle_Area.java
@@ -0,0 +1,20 @@
+package DSA_conditinals;
+
+import java.util.Scanner;
+
+public class Circle_Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter radius of the circle: ");
+        double rad = input.nextInt();
+        //Area of the circle in integer only
+        int ans1 =(int) area(rad);
+        double ans2 = area(rad);
+        System.out.println("Areaof the Circle: "+ans1);
+        System.out.println("Areaof the Circle: "+ans2);
+    }
+    static double area(double radius){
+        double crclarea = Math.PI*Math.pow(radius,2);
+        return crclarea;
+    }
+}
