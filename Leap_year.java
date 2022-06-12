9dc76bc777f5fc66eb9de87910aed73e29341892
Prabhatroy03 <101659036+Prabhatroy03@users.noreply.github.com>
2022-06-12 09:11:09 +0530

    Update Leap_year.java

--- Leap_year.java
+++ Leap_year.java
@@ -6,9 +6,6 @@ import java.util.Scanner;
 
 public class Leap_year {
     public static void main(String[] args) {
-
-
-
         Scanner in = new Scanner(System.in);
         int year = in.nextInt();
         if(year % 4==0){
@@ -18,11 +15,6 @@ public class Leap_year {
             System.out.println("Not leap year: "+year);
         }
     }
-
 }
 
-                2.Take two numbers and print the sum of both.
-                        3.Take a number as input and print the multiplication table for it.
-                        4.Take 2 numbers as inputs and find their HCF and LCM.
-                        5.Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
-                        */
+              
